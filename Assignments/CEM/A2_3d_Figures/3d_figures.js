ac8471e83bc56fa23c833e3edbb90a0229e0cc03
Octavio <octavio.navarro@tec.mx>
2020-02-21 17:21:00 -0600

    Added the second assignment

---
+++ Assignments/CEM/A2_3d_Figures/3d_figures.js
@@ -0,0 +1,167 @@
+var projectionMatrix;
+
+var shaderProgram, shaderVertexPositionAttribute, shaderVertexColorAttribute, 
+    shaderProjectionMatrixUniform, shaderModelViewMatrixUniform;
+
+var duration = 5000; // ms
+
+// Attributes: Input variables used in the vertex shader. Since the vertex shader is called on each vertex, these will be different every time the vertex shader is invoked.
+// Uniforms: Input variables for both the vertex and fragment shaders. These do not change values from vertex to vertex.
+// Varyings: Used for passing data from the vertex shader to the fragment shader. Represent information for which the shader can output different value for each vertex.
+var vertexShaderSource =    
+    "    attribute vec3 vertexPos;\n" +
+    "    attribute vec4 vertexColor;\n" +
+    "    uniform mat4 modelViewMatrix;\n" +
+    "    uniform mat4 projectionMatrix;\n" +
+    "    varying vec4 vColor;\n" +
+    "    void main(void) {\n" +
+    "		// Return the transformed and projected vertex value\n" +
+    "        gl_Position = projectionMatrix * modelViewMatrix * \n" +
+    "            vec4(vertexPos, 1.0);\n" +
+    "        // Output the vertexColor in vColor\n" +
+    "        vColor = vertexColor;\n" +
+    "    }\n";
+
+// precision lowp float
+// This determines how much precision the GPU uses when calculating floats. The use of highp depends on the system.
+// - highp for vertex positions,
+// - mediump for texture coordinates,
+// - lowp for colors.
+var fragmentShaderSource = 
+    "    precision lowp float;\n" +
+    "    varying vec4 vColor;\n" +
+    "    void main(void) {\n" +
+    "    gl_FragColor = vColor;\n" +
+    "}\n";
+
+function initWebGL(canvas)
+{
+    var gl = null;
+    var msg = "Your browser does not support WebGL, " +
+        "or it is not enabled by default.";
+    try 
+    {
+        gl = canvas.getContext("experimental-webgl");
+    } 
+    catch (e)
+    {
+        msg = "Error creating WebGL Context!: " + e.toString();
+    }
+
+    if (!gl)
+    {
+        alert(msg);
+        throw new Error(msg);
+    }
+
+    return gl;        
+ }
+
+function initViewport(gl, canvas)
+{
+    gl.viewport(0, 0, canvas.width, canvas.height);
+}
+
+function initGL(canvas)
+{
+    // Create a project matrix with 45 degree field of view
+    projectionMatrix = mat4.create();
+    mat4.perspective(projectionMatrix, Math.PI / 4, canvas.width / canvas.height, 1, 10000);
+}
+
+// TO DO: Create the functions for each of the figures.
+
+function createShader(gl, str, type)
+{
+    var shader;
+    if (type == "fragment") {
+        shader = gl.createShader(gl.FRAGMENT_SHADER);
+    } else if (type == "vertex") {
+        shader = gl.createShader(gl.VERTEX_SHADER);
+    } else {
+        return null;
+    }
+
+    gl.shaderSource(shader, str);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert(gl.getShaderInfoLog(shader));
+        return null;
+    }
+
+    return shader;
+}
+
+function initShader(gl)
+{
+    // load and compile the fragment and vertex shader
+    var fragmentShader = createShader(gl, fragmentShaderSource, "fragment");
+    var vertexShader = createShader(gl, vertexShaderSource, "vertex");
+
+    // link them together into a new program
+    shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+
+    // get pointers to the shader params
+    shaderVertexPositionAttribute = gl.getAttribLocation(shaderProgram, "vertexPos");
+    gl.enableVertexAttribArray(shaderVertexPositionAttribute);
+
+    shaderVertexColorAttribute = gl.getAttribLocation(shaderProgram, "vertexColor");
+    gl.enableVertexAttribArray(shaderVertexColorAttribute);
+    
+    shaderProjectionMatrixUniform = gl.getUniformLocation(shaderProgram, "projectionMatrix");
+    shaderModelViewMatrixUniform = gl.getUniformLocation(shaderProgram, "modelViewMatrix");
+
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert("Could not initialise shaders");
+    }
+}
+
+function draw(gl, objs) 
+{
+    // clear the background (with black)
+    gl.clearColor(0.1, 0.1, 0.1, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.clear(gl.COLOR_BUFFER_BIT  | gl.DEPTH_BUFFER_BIT);
+
+    // set the shader to use
+    gl.useProgram(shaderProgram);
+
+    for(i = 0; i<objs.length; i++)
+    {
+        obj = objs[i];
+        // connect up the shader parameters: vertex position, color and projection/model matrices
+        // set up the buffers
+        gl.bindBuffer(gl.ARRAY_BUFFER, obj.buffer);
+        gl.vertexAttribPointer(shaderVertexPositionAttribute, obj.vertSize, gl.FLOAT, false, 0, 0);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, obj.colorBuffer);
+        gl.vertexAttribPointer(shaderVertexColorAttribute, obj.colorSize, gl.FLOAT, false, 0, 0);
+        
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, obj.indices);
+
+        gl.uniformMatrix4fv(shaderProjectionMatrixUniform, false, projectionMatrix);
+        gl.uniformMatrix4fv(shaderModelViewMatrixUniform, false, obj.modelViewMatrix);
+
+        // Draw the object's primitives using indexed buffer information.
+        // void gl.drawElements(mode, count, type, offset);
+        // mode: A GLenum specifying the type primitive to render.
+        // count: A GLsizei specifying the number of elements to be rendered.
+        // type: A GLenum specifying the type of the values in the element array buffer.
+        // offset: A GLintptr specifying an offset in the element array buffer.
+        gl.drawElements(obj.primtype, obj.nIndices, gl.UNSIGNED_SHORT, 0);
+    }
+}
+
+function run(gl, objs) 
+{
+    // The window.requestAnimationFrame() method tells the browser that you wish to perform an animation and requests that the browser call a specified function to update an animation before the next repaint. The method takes a callback as an argument to be invoked before the repaint.
+    requestAnimationFrame(function() { run(gl, objs); });
+    draw(gl, objs);
+
+    for(i = 0; i<objs.length; i++)
+        objs[i].update();
+}
---
+++ Assignments/CEM/A2_3d_Figures/README.md
@@ -0,0 +1,27 @@
+# A2 3d Figures
+
+Assignment No 2 for the computer graphics course. Modify previous code examples to render the following:
+
+- A 3D pyramid with a pentagon base as in Figure 1. The pyramid has to rotate around the axis [0.1, 1.0, 0.2].
+- A [Dodecahedron](https://en.wikipedia.org/wiki/Regular_dodecahedron), as in Figure 2. It has to rotate around the axis [-0.4, 1.0, 0.1], and around the axis [0.0, 1.0, 0,0].
+- An Octahedron, as in Figure 3. It has to rotate around the axis [0, 1, 0]; it also has to move up and down within the canvas: it moves up until it reaches the top of the canvas, then it moves down until it reaches the bottom, and then it repeats.
+
+**Rubric:**
+
+1. The canvas was adjusted to include the 3 figures. The pyramid at the left of the canvas, the dodecahedron at the center, and the octahedron at the right.
+2. Created an independent function for each figure.
+3. Defined the vertex, color, and index information for each figure.
+4. Each face of each figure was rendered with a different color.
+5. The figures were transformed correctly.
+
+**Deadline: 01/02/2019 1:00pm**
+
+
+<img src="Images/Pyramid.png" width="300">
+<br/>Figure 1: 3D Pyramid.<br/>
+
+<img src="Images/Dodecahedron.jpg" width="500">
+<br/>Figure 2: 3D Dodecahedron.<br/>
+
+<img src="Images/Octahedron.jpg" width="300">
+<br/>Figure 3: 3D Octahedron.
